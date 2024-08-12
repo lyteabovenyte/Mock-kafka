13aa7039d3ba7344caf76ae4e4d5643972621832
lyteabovenyte <lyteabovenyte@gmail.com>
2024-08-12 23:15:05 +0330

    segment type using a store and index file with it's own apis to read from index and store and append to store and index

---
+++ internal/log/segment.go
@@ -0,0 +1,136 @@
+// segment wraps the index and store types, to coordinate
+// operations across the two
+package log
+
+import (
+	"fmt"
+	"os"
+	"path"
+
+	api "github.com/lyteabovenyte/distributed_services_with_go/api/v1"
+	"google.golang.org/protobuf/proto"
+)
+
+type segment struct {
+	store                  *store
+	index                  *index
+	baseOffset, nextOffset uint64
+	config                 Config
+}
+
+// newSegment is called by log when it needs to add a new segment.
+func newSegment(dir string, baseOffset uint64, c Config) (*segment, error) {
+	s := &segment{
+		baseOffset: baseOffset,
+		config:     c,
+	}
+	storeFile, err := os.OpenFile(
+		path.Join(dir, fmt.Sprintf("%d%s", baseOffset, ".store")),
+		os.O_RDWR|os.O_CREATE|os.O_APPEND,
+		0644,
+	)
+	if err != nil {
+		return nil, err
+	}
+	if s.store, err = newStore(storeFile); err != nil {
+		return nil, err
+	}
+	indexFile, err := os.OpenFile(
+		path.Join(dir, fmt.Sprintf("%d%s", baseOffset, ".index")),
+		os.O_RDWR|os.O_CREATE,
+		0644,
+	)
+	if err != nil {
+		return nil, err
+	}
+	if s.index, err = newIndex(indexFile, c); err != nil {
+		return nil, err
+	}
+	if off, _, err := s.index.Read(-1); err != nil {
+		s.nextOffset = baseOffset
+	} else {
+		s.nextOffset = baseOffset + uint64(off) + 1
+	}
+	return s, nil
+}
+
+// Append writes the record to the segment and returns
+// the newly appended record's offset.
+// the log returns the offset to the API response.
+func (s *segment) Append(record *api.Record) (offset uint64, err error) {
+	cur := s.nextOffset
+	record.Offset = cur
+	p, err := proto.Marshal(record)
+	if err != nil {
+		return 0, err
+	}
+	_, pos, err := s.store.Append(p)
+	if err != nil {
+		return 0, err
+	}
+	err = s.index.Write(
+		// index offsets are relative to base offset
+		uint32(s.nextOffset-uint64(s.baseOffset)),
+		pos,
+	)
+	if err != nil {
+		return 0, err
+	}
+	s.nextOffset++
+	return cur, nil
+}
+
+// Read returns the record for the given offset.
+func (s *segment) Read(off uint64) (*api.Record, error) {
+	_, pos, err := s.index.Read(int64(off - s.baseOffset))
+	if err != nil {
+		return nil, err
+	}
+	p, err := s.store.Read(pos)
+	if err != nil {
+		return nil, err
+	}
+	record := &api.Record{}
+	err = proto.Unmarshal(p, record)
+	return record, err
+}
+
+// IsMaxed returns whether the segment has reached its max size.
+func (s *segment) IsMaxed() bool {
+	return s.store.size >= s.config.Segment.MaxStoreBytes ||
+		s.index.size >= s.config.Segment.MaxIndexBytes
+}
+
+// Remove closes the segment and removes the index and store files.
+func (s *segment) Remove() error {
+	if err := s.Close(); err != nil {
+		return err
+	}
+	if err := os.Remove(s.index.Name()); err != nil {
+		return err
+	}
+	if err := os.Remove(s.store.Name()); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Close calls the Close methods on the index and store files associated with the segment.
+func (s *segment) Close() error {
+	if err := s.index.Close(); err != nil {
+		return err
+	}
+	if err := s.store.Close(); err != nil {
+		return err
+	}
+	return nil
+}
+
+// nearestMultiple returns the nearest and lesser multiple of k in j.
+// making sure we stay under the user's disk capacity.
+func (s *segment) nearestMultiple(j, k uint64) uint64 {
+	if j >= 0 {
+		return (j / k) * k
+	}
+	return ((j - k + 1) / k) * k
+}
