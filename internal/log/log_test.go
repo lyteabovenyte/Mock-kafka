204a50b540a53eef8358e2a4b683ffb89aa867a7
lyteabovenyte <lyteabovenyte@gmail.com>
2024-08-14 05:11:51 +0330

    fixed some functionalities

--- internal/log/log_test.go
+++ internal/log/log_test.go
@@ -2,6 +2,7 @@ package log
 
 import (
 	"io/ioutil"
+	"os"
 	"testing"
 
 	api "github.com/lyteabovenyte/distributed_services_with_go/api/v1"
@@ -13,29 +14,29 @@ import (
 // to centralize the creation of the log.
 func TestLog(t *testing.T) {
 	for scenario, fn := range map[string]func(
-		t *testing.T, log *Log
+		t *testing.T, log *Log,
 	){
-		"append and read a record succeded", testAppendRead,
-		"offset out of range error", testOutOfRangeErr,
-		"init with existing setgment", testInitExisting,
-		"reader", testReader,
-		"truncate", testTruncate,
-	}{
-		t.Run(scenario, func(t *testing.T){
+		"append and read a record succeded": testAppendRead,
+		"offset out of range error":         testOutOfRangeErr,
+		"init with existing setgment":       testInitExisting,
+		"reader":                            testReader,
+		"truncate":                          testTruncate,
+	} {
+		t.Run(scenario, func(t *testing.T) {
 			dir, err := ioutil.TempDir("", "store-test")
 			require.NoError(t, err)
 			defer os.RemoveAll(dir)
 			c := Config{}
 			c.Segment.MaxStoreBytes = 32
 			log, err := NewLog(dir, c)
-			require.NoError(err)
+			require.NoError(t, err)
 			fn(t, log)
 		})
 	}
 }
 
 func testAppendRead(t *testing.T, log *Log) {
-	append := &api.Record {
+	append := &api.Record{
 		Value: []byte("hello world"),
 	}
 	off, err := log.Append(append)
@@ -47,7 +48,7 @@ func testAppendRead(t *testing.T, log *Log) {
 	require.Equal(t, read.Value, append.Value)
 }
 
-func testOutOfRangeErr(t *testing.T) {
+func testOutOfRangeErr(t *testing.T, log *Log) {
 	read, err := log.Read(1)
 	require.Error(t, err)
 	require.Nil(t, read)
@@ -66,7 +67,7 @@ func testInitExisting(t *testing.T, log *Log) {
 	off, err := log.LowestOffset()
 	require.NoError(t, err)
 	require.Equal(t, uint64(0), off)
-	off, err := log.HighestOffset()
+	off, err = log.HighestOffset()
 	require.NoError(t, err)
 	require.Equal(t, uint64(2), off)
 
