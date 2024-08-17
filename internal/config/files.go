e16f2f8aa37a80bbfc70be87aa0cb793aefe50ee
lyteabovenyte <lyteabovenyte@gmail.com>
2024-08-18 02:16:26 +0330

    added SetUpTLSConfig func to setup our *tls.Config for each of the config files + configFile func to get each of the filepath for certs

---
+++ internal/config/files.go
@@ -0,0 +1,24 @@
+// containes certificates' filepaths as variables to make referencing them easy
+package config
+
+import (
+	"os"
+	"path/filepath"
+)
+
+var (
+	CAFile         = configFile("ca.pem")
+	ServerCertFile = configFile("server.pem")
+	ServerKeyFile  = configFile("server-key.pem")
+)
+
+func configFile(filename string) string {
+	if dir := os.Getenv("CONFIG_DIR"); dir != "" {
+		return filepath.Join(dir, filename)
+	}
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		panic(err)
+	}
+	return filepath.Join(homeDir, "/Github/distributed_services_with_go/.certs", filename)
+}
