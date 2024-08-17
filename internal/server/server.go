7f2593f790cff789d9b15807862a4532ddbd2082
lyteabovenyte <lyteabovenyte@gmail.com>
2024-08-18 02:14:33 +0330

    added serverOption to our NewGRPCServer func for encrypted connection with TLSCreds

--- internal/server/server.go
+++ internal/server/server.go
@@ -11,12 +11,12 @@ type Config struct {
 	CommitLog CommitLog
 }
 
-// TODO!
 var _ api.LogServer = (*grpcServer)(nil)
 
+// TODO! add TLS credentials to the server.
 // NewGRPCServer provides the users a way to instantiate out service.
-func NewGRPCServer(config *Config) (*grpc.Server, error) {
-	gsrv := grpc.NewServer()
+func NewGRPCServer(config *Config, opts ...grpc.ServerOption) (*grpc.Server, error) {
+	gsrv := grpc.NewServer(opts...)
 	srv, err := newgrpcServer(config)
 	if err != nil {
 		return nil, err
