8f7e6af49862ec085169fcf93f14fb5ec4d3a0e2
lyteabovenyte <lyteabovenyte@gmail.com>
2024-08-21 04:14:18 +0330

    serf cluster membership for our nodes for service to service discovery

---
+++ internal/discovery/membership.go
@@ -0,0 +1,135 @@
+// discovery package tells us when a server
+// joined or left the cluster of Serf's nodes
+// and what its ID and address are with as little API
+// as possible.
+package discovery
+
+import (
+	"net"
+
+	"github.com/hashicorp/serf/serf"
+	"go.uber.org/zap"
+)
+
+// Membership is the type wrapping Serf to provide
+// discovery and cluster membership to our service.
+type Membership struct {
+	Config
+	handler Handler
+	serf    *serf.Serf
+	events  chan serf.Event
+	logger  *zap.Logger
+}
+
+type Config struct {
+	NodeName       string
+	BindAddr       string
+	Tags           map[string]string
+	StartJoinAddrs []string
+}
+
+type Handler interface {
+	Join(name, addr string) error
+	Leave(name string) error
+}
+
+func New(handler Handler, config Config) (*Membership, error) {
+	c := &Membership{
+		Config:  config,
+		handler: handler,
+		logger:  zap.L().Named("membership"),
+	}
+	if err := c.setupSerf(); err != nil {
+		return nil, err
+	}
+	return c, nil
+}
+
+// setupSerf creates and configure a Serf instance and starts
+// the eventHandler() goroutine to handle Serf's events.
+func (m *Membership) setupSerf() (err error) {
+	addr, err := net.ResolveTCPAddr("tcp", m.BindAddr)
+	if err != nil {
+		return err
+	}
+	config := serf.DefaultConfig()
+	config.Init()
+	config.MemberlistConfig.BindAddr = addr.IP.String()
+	config.MemberlistConfig.BindPort = addr.Port
+	m.events = make(chan serf.Event)
+	config.EventCh = m.events
+	config.Tags = m.Tags
+	config.NodeName = m.Config.NodeName
+	m.serf, err = serf.Create(config)
+	if err != nil {
+		return err
+	}
+	go m.eventHandler()
+	if m.StartJoinAddrs != nil {
+		_, err = m.serf.Join(m.StartJoinAddrs, true)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (m *Membership) eventHandler() {
+	for e := range m.events {
+		switch e.EventType() {
+		case serf.EventMemberJoin:
+			for _, member := range e.(serf.MemberEvent).Members {
+				if m.isLocal(member) {
+					continue // preventing the server to act on itself and replicate itself.
+				}
+				m.handleJoin(member)
+			}
+		case serf.EventMemberFailed, serf.EventMemberLeave:
+			for _, member := range e.(serf.MemberEvent).Members {
+				if m.isLocal(member) {
+					return // preventing the server to act on itself and replicate itself.
+				}
+				m.handleLeave(member)
+			}
+		}
+	}
+}
+
+func (m *Membership) handleJoin(member serf.Member) {
+	if err := m.handler.Join(
+		member.Name,
+		member.Tags["rpc-addr"],
+	); err != nil {
+		m.logError(err, "failed to join", member)
+	}
+}
+
+func (m *Membership) handleLeave(member serf.Member) {
+	if err := m.handler.Leave(
+		member.Name,
+	); err != nil {
+		m.logError(err, "failed to leave", member)
+	}
+}
+
+func (m *Membership) isLocal(member serf.Member) bool {
+	return m.serf.LocalMember().Name == member.Name
+}
+
+// Memebers returns a point-in-time snapshot of the cluster's Serf mamber
+func (m *Membership) Members() []serf.Member {
+	return m.serf.Members()
+}
+
+func (m *Membership) Leave() error {
+	return m.serf.Leave()
+}
+
+func (m *Membership) logError(err error, msg string, member serf.Member) {
+	m.logger.Error(
+		msg,
+		zap.Error(err),
+		zap.String("name", member.Name),
+		zap.String("rpc_addr", member.Tags["rpc_addr"]),
+	)
+}
---
+++ internal/discovery/membership_test.go
@@ -0,0 +1,5 @@
+package discovery_test
+
+import (
+	. "github.com/lyteabovenyte/distributed_services_with_go/internal/dicovery"
+)
