3e9635aba65b8d6d0ce2a05915b392c52c952cbb
lyteabovenyte <lyteabovenyte@gmail.com>
2024-08-26 13:29:53 +0330

    added raft config to log's configuration structure

--- internal/log/config.go
+++ internal/log/config.go
@@ -2,7 +2,17 @@
 // centralize the log's configuration with config struct
 package log
 
+import (
+	"github.com/hashicorp/raft"
+)
+
 type Config struct {
+	Raft struct {
+		raft.Config
+		StreamLayer *StreamLayer
+		Bootstrap   bool
+	}
+
 	Segment struct {
 		MaxStoreBytes uint64
 		MaxIndexBytes uint64
